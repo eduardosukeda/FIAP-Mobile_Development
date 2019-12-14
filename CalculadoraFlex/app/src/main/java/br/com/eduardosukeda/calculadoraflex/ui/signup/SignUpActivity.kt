e33ebe665c550aa5b1729e49c5773cc91e9f1c1d
Logon Aluno <logonaluno@local.com>
2019-12-14 11:15:26 -0300

    Initial Commit

---
+++ CalculadoraFlex/app/src/main/java/br/com/eduardosukeda/calculadoraflex/ui/signup/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package br.com.eduardosukeda.calculadoraflex.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

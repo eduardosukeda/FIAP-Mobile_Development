7c5acb15a7538c2bd19f7220f2428bd9d2578084
Logon Aluno <logonaluno@local.com>
2019-12-14 12:48:53 -0300

    Adicionado firebase ao projeto

--- CalculadoraFlex/app/src/main/java/br/com/eduardosukeda/calculadoraflex/ui/signup/SignUpActivity.kt
+++ CalculadoraFlex/app/src/main/java/br/com/eduardosukeda/calculadoraflex/ui/signup/SignUpActivity.kt
@@ -2,10 +2,12 @@ package br.com.eduardosukeda.calculadoraflex.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.eduardosukeda.calculadoraflex.R
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
     }
 }
